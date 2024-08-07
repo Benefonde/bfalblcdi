3c035ae1aa0bd987a37e539d5d3417d164afa6fb
Benefonde <marysiahubcio@gmail.com>
2024-08-07 11:04:22 +0200

    hey vro what version am i GAMING on??
    this one

---
+++ Assets/Scripts/VersionText.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class VersionText : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+        t[0].text = $"BFALBLCDI {Application.version}";
+        t[1].text = $"BFALBLCDI {Application.version}";
+    }
+
+    public TMP_Text[] t;
+}
